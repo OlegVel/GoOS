0c4e0acf682eeeca0795df928e98a19d7798a797
Oleg Velihurskyi <olegvelig@gmail.com>
2019-07-25 13:25:10 +0300

    Adding JSON testing

---
+++ front/cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("files"))
+	http.Handle("/files/", http.StripPrefix("/files/", fileServer))
+	http.Handle("/", http.FileServer(http.Dir("front")))
+	http.HandleFunc("/api", handleApi())
+
+
+
+	if err := http.ListenAndServe(":5000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type testStruct struct {
+	Test string
+}
+
+func handleApi() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		decoder := json.NewDecoder(req.Body)
+		var t testStruct
+		err := decoder.Decode(&t)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(t.Test)
+
+
+		fmt.Println("Hello world")
+	}
+}
