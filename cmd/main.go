2b6b1718bd4897c6667993b320f1a7bcf5a35aab
OlegVel <olegvelig@gmail.com>
2019-07-28 11:59:01 +0300

    Removing old files

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
-package main
+package cmd
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -8,7 +10,28 @@ import (
 func main() {
 	fileServer := http.FileServer(http.Dir("files"))
 	http.Handle("/files/", http.StripPrefix("/files/", fileServer))
+	http.Handle("/", http.FileServer(http.Dir("front")))
+	http.HandleFunc("/api", handleApi())
+
 	if err := http.ListenAndServe(":5000", nil); err != nil {
 		log.Fatal(err)
+	}
+}
+
+type testStruct struct {
+	Test string
+}
+
+func handleApi() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		decoder := json.NewDecoder(req.Body)
+		var t testStruct
+		err := decoder.Decode(&t)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(t.Test)
+
+		fmt.Println("Hello world")
 	}
 }
--- front/javascript/script.js
+++ front/javascript/script.js
@@ -1,3 +1,19 @@
 function myFunction() {
   document.getElementById('right').textContent='hello';
 }
+
+function jsonFunc() {
+  var xhr = new XMLHttpRequest();
+  var url = 'http://localhost:5000/api';
+
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var json = JSON.parse(xhr.responseText);
+      console.log(json.test + ", " + json.password);
+    }
+  };
+  var data = JSON.stringify({"test": "hey@mail.com", "password": "101010"});
+  xhr.send(data);
+}
